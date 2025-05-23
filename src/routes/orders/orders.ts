85ea19a5322c3f0167f4ca00ee127d6a2dc3551e
M Sufyan Shaikh <sufyan.souloftware@gmail.com>
2025-05-23 12:44:28 +0500

    orders fixing

--- src/routes/orders/orders.ts
+++ src/routes/orders/orders.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// src/routes/orders.ts
-import { Router } from 'express';
-import { createOrderController, getOrdersController } from '../../controllers/orders/orders';
-import { createOrderValidation } from '../../validations/orders/orders';
-import { validate } from '../middlewares/validate';
-
-const router = Router();
-
-router.post('/orders', createOrderValidation, validate, createOrderController);
-router.get('/orders', getOrdersController);
-
-export default router;
-=======
 import express from 'express'
 import validateRequest from '../../middlewares/validateRequest.middleware'
 import authRequired from '../../middlewares/authRequired.middleware'
@@ -34,4 +20,3 @@ router.post(
 )
 
 export default router
->>>>>>> ae61c98b84bbb5d60d8c73d10e8a42282244eefb
--- src/validations/orders/orders.ts
+++ src/validations/orders/orders.ts
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// src/validations/orders.ts
-import { body } from 'express-validator';
-
-export const createOrderValidation = [
-  body('userId').isInt().withMessage('User ID must be an integer'),
-  body('productId').isInt().withMessage('Product ID must be an integer'),
-  body('quantity').isInt({ gt: 0 }).withMessage('Quantity must be greater than 0'),
-  body('totalPrice').isFloat({ gt: 0 }).withMessage('Total price must be greater than 0'),
-];
-=======
 import Joi from 'joi'
 
 const getAllOrders = Joi.object({
@@ -30,4 +19,3 @@ export default {
   getAllOrders,
   createOrder
 }
->>>>>>> ae61c98b84bbb5d60d8c73d10e8a42282244eefb
